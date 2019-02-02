c58c6b5d90e9c681181c6cdb6338ba676b913711
Stewart-McDonald <2247114m@student.gla.ac.uk>
2019-02-02 17:30:24 +0000

    chapter 4 complete.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -19,6 +19,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -27,4 +29,5 @@ urlpatterns = [
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
