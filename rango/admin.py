3eb649cf9f01fdd1b6d7188e7d644f043530a1b7
Stewart <2247114m@student.gla.ac.uk>
2019-02-06 23:10:51 +0000

    chapter 5 finished - pre testing

--- rango/admin.py
+++ rango/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from rango.models import Category, Page
 # Register your models here.
 
+
+class PageAdmin():
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Page)
+
+#admin.site.register(Page, PageAdmin)
 admin.site.register(Category)
-admin.sites.register(Page)
+
