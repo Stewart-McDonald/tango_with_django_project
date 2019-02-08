e424df164bb1e1dc5ffd9038c292b99913c974b4
Stewart <2247114m@student.gla.ac.uk>
2019-02-08 17:51:02 +0000

    chapter 9 halfway

--- rango/views.py
+++ rango/views.py
@@ -9,13 +9,18 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
+
 
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by("-likes")[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    print(category_list, page_list)
     context_dict = {'categories': category_list, 'pages': page_list}
-    return render(request, 'rango/index.html', context=context_dict)
+    response = render(request, 'rango/index.html', context_dict)
+    visitor_cookie_handler(request, response)
+    print(category_list, page_list)
+    return response
 
 # Return a rendered response to send to the client.
 # We make use of the shortcut function to make our lives easier.
@@ -28,6 +33,9 @@ def about(request):
     print(request.method)
     # prints out the user name, if no one is logged in it prints `AnonymousUser`
     print(request.user)
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     return render(request, 'rango/about.html', context=context_dict)
 
 
@@ -186,9 +194,31 @@ def some_view(request):
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
 
+
 @login_required
 def user_logout(request):
     # Since we know the user is logged in, we can now just log them out.
     logout(request)
     # Take the user back to the homepage.
     return HttpResponseRedirect(reverse('index'))
+
+
+def visitor_cookie_handler(request, response):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        # Set the last visit cookie
+        response.set_cookie('last_visit', last_visit_cookie)
+        # Update/set the visits cookie
+    response.set_cookie('visits', visits)
+
